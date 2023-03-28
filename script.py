924d8c8cca34daddf5743dc2d4be08aed8d37ab1
Maxime Coriton <maxime.coriton@gmail.com>
2023-03-28 20:11:59 +0200

    comments

--- script.py
+++ script.py
@@ -7,14 +7,14 @@ intents = discord.Intents().all()
 client = discord.Client(intents=intents)
 
 channel_id = os.getenv("CHANNEL_ID") # l'ID du channel sur lequel vous souhaitez que le bot fonctionne
-bot_token = os.getenv("BOT_TOKEN")
+bot_token = os.getenv("BOT_TOKEN") # le token du bot
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.channel.id == int(channel_id) : # Remplacez CHANNEL_ID par l'ID du channel sur lequel vous souhaitez que le bot fonctionne
+    if message.channel.id == int(channel_id) : 
         twitter_url_pattern = r'https?://(www\.)?twitter\.com/.+?/status/\d+'
         twitter_url_regex = re.compile(twitter_url_pattern)
 
@@ -25,4 +25,4 @@ async def on_message(message):
             await message.reply(reply_message, mention_author=False)
             await message.edit(suppress=True)
 
-client.run(bot_token) # Remplacez BOT_TOKEN par le jeton de votre bot Discord
+client.run(bot_token)
